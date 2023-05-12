d8505fa4b505fb8ea38113cf6d7d1b59b5cbbaf7
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-05-12 06:30:10 +0330

    String package added with initial functions : zz_fn_SplitString and zz_fn_NthItem

---
+++ MsSql/Packages/String/All.sql
@@ -0,0 +1,2 @@
+EXEC zzz_sp_Deploy 'zz_Sample/zz_sp_Add.sql';
+EXEC zzz_sp_Deploy 'zz_Sample/zz_sp_Individual.sql';
---
+++ MsSql/Packages/String/zz_fn_SplitString.sql
@@ -0,0 +1,21 @@
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		Mohsen Mirshahreza
+-- Create date: 2023-05-13
+-- Description:	To split a separated string to rows
+-- =============================================
+CREATE OR ALTER FUNCTION zz_fn_SplitString 
+(
+	@String NVARCHAR(4000),@Splitter CHAR(1)
+)
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT value FROM STRING_SPLIT(ISNULL(@String,''), ISNULL(@Splitter,','))
+)
+GO
+
