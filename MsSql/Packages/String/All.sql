9c84a86b40fed06f85221f3d2615aef7193228da
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-05-18 09:45:22 +0330

    zz_fn_Fix2Char added

--- MsSql/Packages/String/All.sql
+++ MsSql/Packages/String/All.sql
@@ -1,3 +1,4 @@
 EXEC zzz_sp_Deploy 'String/zz_fn_SplitString.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_NthItem.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_Trim.sql';
+EXEC zzz_sp_Deploy 'String/zz_fn_Fix2Char';
