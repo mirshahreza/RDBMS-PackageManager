236cfdbb3535295518d852247b1c51728f05ebf3
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-05-27 20:11:06 +0330

    - zz_fn_CountWord added

--- MsSql/Packages/String/All.sql
+++ MsSql/Packages/String/All.sql
@@ -2,3 +2,4 @@ EXEC zzz_sp_Deploy 'String/zz_fn_SplitString.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_NthItem.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_Trim.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_Fix2Char.sql';
+EXEC zzz_sp_Deploy 'String/zz_fn_CountWord.sql';
