22c8bf98d34e676ce18df44dc8fdbe5c43909695
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-05-27 20:15:33 +0330

    All.sql edited

--- MsSql/Packages/String/All.sql
+++ MsSql/Packages/String/All.sql
@@ -2,4 +2,5 @@ EXEC zzz_sp_Deploy 'String/zz_fn_SplitString.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_NthItem.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_Trim.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_Fix2Char.sql';
+EXEC zzz_sp_Deploy 'String/zz_fn_CountChar.sql';
 EXEC zzz_sp_Deploy 'String/zz_fn_CountWord.sql';
