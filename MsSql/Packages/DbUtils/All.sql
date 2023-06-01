564b27b90b575bec2add06a5d58f1a9d2841d01f
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-01 12:17:35 +0330

    - zz_vw_UserTablesViewsFields added

--- MsSql/Packages/DbUtils/All.sql
+++ MsSql/Packages/DbUtils/All.sql
@@ -2,3 +2,4 @@ EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserObjectsDetails.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserObjectsOverview.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesIndexes.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesOverview.sql';
+EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesViewsFields.sql';
