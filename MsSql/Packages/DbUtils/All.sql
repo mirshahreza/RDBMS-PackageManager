f2595790ce2451b78954b2ac67e3c210ab45e814
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-02 07:32:56 +0330

    - zz_sp_CreateField added

--- MsSql/Packages/DbUtils/All.sql
+++ MsSql/Packages/DbUtils/All.sql
@@ -6,5 +6,7 @@ EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesViewsFields.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesFks.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_sp_CreateTableIdentity.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_sp_CreateTableGuid.sql';
+EXEC zzz_sp_Deploy 'DbUtils/zz_sp_CreateField.sql';
+
 
 
