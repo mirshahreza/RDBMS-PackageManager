3c0d3ea90aac61e96ee57fedc7bb9d24b4c9c024
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-02 07:03:09 +0330

    - zz_sp_CreateTableIdentity added

--- MsSql/Packages/DbUtils/All.sql
+++ MsSql/Packages/DbUtils/All.sql
@@ -4,4 +4,6 @@ EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesIndexes.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesOverview.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesViewsFields.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesFks.sql';
+EXEC zzz_sp_Deploy 'DbUtils/zz_sp_CreateTableIdentity.sql';
+
 
