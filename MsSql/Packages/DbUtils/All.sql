d109d8b8f5c66dd0a5049ba35652af8146eb79ac
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-02 06:13:54 +0330

    -- zz_vw_UserTablesFks added

--- MsSql/Packages/DbUtils/All.sql
+++ MsSql/Packages/DbUtils/All.sql
@@ -3,3 +3,5 @@ EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserObjectsOverview.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesIndexes.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesOverview.sql';
 EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesViewsFields.sql';
+EXEC zzz_sp_Deploy 'DbUtils/zz_vw_UserTablesFks.sql';
+
