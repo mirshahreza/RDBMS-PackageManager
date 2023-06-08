0088fd82746a82329e3081e7e7d2f6336b6f8d72
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-08 08:28:51 +0330

    - Rename

--- MsSql/Packages/DbUtils/All.sql
+++ MsSql/Packages/DbUtils/All.sql
@@ -2,11 +2,11 @@ EXEC zzz_Deploy 'DbUtils/zz_UserObjectsDetails.sql';
 EXEC zzz_Deploy 'DbUtils/zz_UserObjectsOverview.sql';
 EXEC zzz_Deploy 'DbUtils/zz_UserTablesIndexes.sql';
 EXEC zzz_Deploy 'DbUtils/zz_UserTablesOverview.sql';
-EXEC zzz_Deploy 'DbUtils/zz_UserTablesViewsFields.sql';
+EXEC zzz_Deploy 'DbUtils/zz_UserTablesViewsColumns.sql';
 EXEC zzz_Deploy 'DbUtils/zz_UserTablesFks.sql';
 EXEC zzz_Deploy 'DbUtils/zz_CreateTableIdentity.sql';
 EXEC zzz_Deploy 'DbUtils/zz_CreateTableGuid.sql';
-EXEC zzz_Deploy 'DbUtils/zz_CreateField.sql';
+EXEC zzz_Deploy 'DbUtils/zz_CreateColumn.sql';
 EXEC zzz_Deploy 'DbUtils/zz_DropColumn.sql';
 EXEC zzz_Deploy 'DbUtils/zz_DropTable.sql';
 
