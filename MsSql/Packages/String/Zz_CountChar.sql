7f3551a49785ee1aea137bd15edbabef859fa9c8
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-14 17:11:04 +0330

    Names edited

--- MsSql/Packages/String/Zz_CountChar.sql
+++ MsSql/Packages/String/Zz_CountChar.sql
@@ -1,19 +1,32 @@
 -- =============================================
 -- Author:		Mohsen Mirshahreza
 -- Create date: 2023-05-18
--- Description:	Calculate count of existing a word in a string
+-- Description:	Calculate count of existing a char in a string
 -- =============================================
-CREATE OR ALTER FUNCTION [dbo].[Zz_CountWord] 
+CREATE OR ALTER FUNCTION [dbo].[Zz_CountChar] 
 ( 
-    @InputPhrase NVARCHAR(4000), 
-    @SearchWord NVARCHAR(64) 
+    @pInput VARCHAR(1000), 
+    @pSearchChar CHAR(1) 
 )
 RETURNS INT
 BEGIN
 
-  RETURN
-    (  
-        SELECT (DATALENGTH(@InputPhrase)-DATALENGTH(REPLACE(@InputPhrase,@SearchWord,'')))/DATALENGTH(@SearchWord) 
-    );
+    DECLARE @vInputLength        INT
+    DECLARE @vIndex              INT
+    DECLARE @vCount              INT
+
+    SET @vCount = 0
+    SET @vIndex = 1
+    SET @vInputLength = LEN(@pInput)
+
+    WHILE @vIndex <= @vInputLength
+    BEGIN
+        IF SUBSTRING(@pInput, @vIndex, 1) = @pSearchChar
+            SET @vCount = @vCount + 1
+
+        SET @vIndex = @vIndex + 1
+    END
+
+    RETURN @vCount
 
 END
