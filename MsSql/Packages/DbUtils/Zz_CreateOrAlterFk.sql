c4d98df24c068f4a967b84bf4de855ed8e2e51ad
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-22 01:44:18 +0330

    Zz_CreateOrAlterFk added bugfix for DropColumn

---
+++ MsSql/Packages/DbUtils/Zz_CreateOrAlterFk.sql
@@ -0,0 +1,42 @@
+-- =============================================
+-- Author:		Mohsen Mirshahreza
+-- Create date: 2023-06-02
+-- Description:	Creates or Alters a Fk
+-- =============================================
+CREATE OR ALTER PROCEDURE [dbo].[Zz_CreateOrAlterFk]
+	@FkName VARCHAR(128),
+	@BaseTableName VARCHAR(128), @BaseColumnName VARCHAR(64) , 
+	@TargetTableName VARCHAR(128), @TargetColumnName VARCHAR(64) , 
+	@EnforceRelation BIT=1 
+AS
+
+BEGIN
+
+	DECLARE @S NVARCHAR(4000);
+
+	IF(@FkName IS NOT NULL AND @FkName!='')
+	BEGIN
+		SET @S = '
+ALTER TABLE '+@BaseTableName+'
+DROP CONSTRAINT '+@FkName+'
+;
+		';
+	END
+
+	SET @S = @S + '
+ALTER TABLE '+@BaseTableName+'
+ADD CONSTRAINT '+@FkName+' FOREIGN KEY ('+@BaseColumnName+')
+    REFERENCES '+@TargetTableName+' ('+@TargetColumnName+')
+	';
+
+	IF(@EnforceRelation=1)
+	BEGIN
+		SET @S = @S + '
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+	;
+		';
+	END
+
+	EXECUTE SP_EXECUTESQL @S;
+END
