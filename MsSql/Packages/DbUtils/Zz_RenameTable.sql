2018779331e3d2ba8d271e3f0010f43aca907ea7
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-06-22 17:21:06 +0330

    Zz_RenameTable added

---
+++ MsSql/Packages/DbUtils/Zz_RenameTable.sql
@@ -0,0 +1,16 @@
+-- =============================================
+-- Author:		Mohsen Mirshahreza
+-- Create date: 2023-06-21
+-- Description:	Rename a table 
+-- =============================================
+CREATE OR ALTER PROCEDURE [dbo].[Zz_RenameTable]
+	@TableName VARCHAR(128),@NewName VARCHAR(128)
+AS
+
+BEGIN
+
+	DECLARE @S NVARCHAR(4000);
+	SET @S = 'EXEC SP_RENAME '''+@TableName+''','''+@NewName+''';';
+	EXECUTE SP_EXECUTESQL @S;
+
+END
