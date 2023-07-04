c2591301e0198d13b3e0672d5a792ae74674e40d
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-07-04 12:02:05 +0330

    Zz_DropFk added

---
+++ MsSql/Packages/DbUtils/Zz_DropFk.sql
@@ -0,0 +1,25 @@
+-- =============================================
+-- Author:		Mohsen Mirshahreza
+-- Create date: 2023-07-04
+-- Description:	Drops a Fk
+-- =============================================
+CREATE OR ALTER PROCEDURE [dbo].[Zz_DropFk]
+	@FkName VARCHAR(128),
+	@BaseTableName VARCHAR(128)
+AS
+
+BEGIN
+
+	DECLARE @S NVARCHAR(4000);
+
+	IF(@FkName IS NOT NULL AND @FkName!='')
+	BEGIN
+		SET @S = '
+ALTER TABLE '+@BaseTableName+'
+DROP CONSTRAINT IF EXISTS '+@FkName+'
+;
+		';
+	END
+
+	EXECUTE SP_EXECUTESQL @S;
+END
