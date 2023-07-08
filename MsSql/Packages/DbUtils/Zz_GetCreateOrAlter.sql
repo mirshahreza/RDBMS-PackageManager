e86c6f966080e75f2414fce2583637bf4de34d91
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-07-08 23:59:31 +0330

    Zz_GetCreateOrAlter added

---
+++ MsSql/Packages/DbUtils/Zz_GetCreateOrAlter.sql
@@ -0,0 +1,31 @@
+-- =============================================
+-- Author:		Mohsen Mirshahreza
+-- Create date: 2023-07-08
+-- Description:	Generates Create Or Alter
+-- =============================================
+CREATE OR ALTER PROCEDURE [dbo].[Zz_GetCreateOrAlter]
+	@ObjectName VARCHAR(128)
+AS
+
+BEGIN
+
+SELECT TOP 1 CreateOrAlter FROM 
+(
+	SELECT ObjectDef,ObjectNameCharIndex,ObjectDefUntilName,MainPhrase
+		,CHARINDEX (MainPhrase ,ObjectDefUntilName) GG
+		,REPLACE(ObjectDef,MainPhrase,(REPLACE(MainPhrase,'CREATE ','CREATE OR UPDATE'))) AS CreateOrAlter
+	FROM (
+		SELECT ObjectDef,ObjectNameCharIndex,ObjectDefUntilName 
+			, LTRIM(RTRIM(REVERSE(SUBSTRING(REVERSE(ObjectDefUntilName),0,CHARINDEX(' ETAERC', REVERSE(ObjectDefUntilName))+7)))) AS MainPhrase
+		FROM (
+			SELECT
+				DEFINITION AS ObjectDef,
+				CHARINDEX('Zz_CreateTableGuid', DEFINITION) AS ObjectNameCharIndex,
+				SUBSTRING(DEFINITION,0,CHARINDEX('Zz_CreateTableGuid', DEFINITION)) AS ObjectDefUntilName
+			FROM SYS.SQL_MODULES 
+			WHERE OBJECT_ID= OBJECT_ID('Zz_CreateTableGuid')
+		) T
+	) T
+) T
+
+END
