3ef3c25329039c848ac9655557a99e36c1cdab09
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-07-13 09:57:12 +0330

    Default added to alter column

--- MsSql/Packages/DbUtils/Zz_AlterColumn.sql
+++ MsSql/Packages/DbUtils/Zz_AlterColumn.sql
@@ -4,21 +4,39 @@
 -- Description:	alter a field
 -- =============================================
 CREATE OR ALTER PROCEDURE [dbo].[Zz_AlterColumn]
-	@TableName VARCHAR(128), @ColumnName VARCHAR(64), @ColumnTypeSize VARCHAR(64), @AllowNull BIT = 1
+	@TableName VARCHAR(128), @ColumnName VARCHAR(64), @ColumnTypeSize VARCHAR(64), @AllowNull BIT = 1,@Default NVARCHAR(256) = ''
 AS
 
 BEGIN
 
 	DECLARE @S NVARCHAR(4000);
-	SET @S = 'ALTER TABLE ' + @TableName + ' ALTER COLUMN ' + @ColumnName + ' ' + @ColumnTypeSize;
+	DECLARE @Allow NVARCHAR(16);
+	DECLARE @DefaultName NVARCHAR(256);
+
 	IF(@AllowNull=1)
 	BEGIN
-		SET @S = @S + ' NULL'
+		SET @Allow = ' NULL'
 	END
 	ELSE
 	BEGIN
-		SET @S = @S + ' NOT NULL'
+		SET @Allow = ' NOT NULL'
 	END
+
+	SET @DefaultName = ISNULL((SELECT TOP 1 Name FROM SYS.DEFAULT_CONSTRAINTS
+	WHERE PARENT_OBJECT_ID = OBJECT_ID(@TableName)
+	AND PARENT_COLUMN_ID = (SELECT COLUMN_ID FROM SYS.COLUMNS
+							WHERE NAME = @ColumnName
+							AND OBJECT_ID = OBJECT_ID(@TableName))
+						),'NODEFAULT');
+
+	SET @S ='
+ALTER TABLE ' + @TableName + ' ALTER COLUMN ' + @ColumnName + ' ' + @ColumnTypeSize + @Allow + ';
+ALTER TABLE ' + @TableName + ' DROP CONSTRAINT IF EXISTS ' + @DefaultName+';
+IF('''+@Default+'''!='''')
+BEGIN
+	ALTER TABLE ' + @TableName + ' ADD CONSTRAINT ' + @ColumnName + '_Default DEFAULT '''+@Default+''' FOR ' + @ColumnName + '
+END
+;';
 
 	EXECUTE SP_EXECUTESQL @S;
 END
