66359c697e8003bb96eb28feaaccb6113789ce64
Mohsen Mirshahreza <mirshahreza@outlook.com>
2023-07-13 10:53:36 +0330

    Rename some objects

--- MsSql/Packages/DbUtils/All.sql
+++ MsSql/Packages/DbUtils/All.sql
@@ -1,10 +1,10 @@
-EXEC Zzz_Deploy 'DbUtils/Zz_UserObjectsDetails.sql';
-EXEC Zzz_Deploy 'DbUtils/Zz_UserObjectsOverview.sql';
-EXEC Zzz_Deploy 'DbUtils/Zz_UserTablesIndexes.sql';
-EXEC Zzz_Deploy 'DbUtils/Zz_UserTablesOverview.sql';
-EXEC Zzz_Deploy 'DbUtils/Zz_UserTablesViewsColumns.sql';
-EXEC Zzz_Deploy 'DbUtils/Zz_UserTablesFks.sql';
-EXEC Zzz_Deploy 'DbUtils/Zz_UserProceduresFunctionsParameters.sql';
+EXEC Zzz_Deploy 'DbUtils/Zz_SelectObjectsDetails.sql';
+EXEC Zzz_Deploy 'DbUtils/Zz_SelectObjectsOverview.sql';
+EXEC Zzz_Deploy 'DbUtils/Zz_SelectTablesIndexes.sql';
+EXEC Zzz_Deploy 'DbUtils/Zz_SelectTablesOverview.sql';
+EXEC Zzz_Deploy 'DbUtils/Zz_SelectTablesViewsColumns.sql';
+EXEC Zzz_Deploy 'DbUtils/Zz_SelectTablesFks.sql';
+EXEC Zzz_Deploy 'DbUtils/Zz_SelectProceduresFunctionsParameters.sql';
 EXEC Zzz_Deploy 'DbUtils/Zz_CreateTableIdentity.sql';
 EXEC Zzz_Deploy 'DbUtils/Zz_CreateTableGuid.sql';
 EXEC Zzz_Deploy 'DbUtils/Zz_CreateColumn.sql';
