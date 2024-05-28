f63c18a96a154f63a6f1fdb862081a7c9212453b
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-05-28 15:13:18 +0330

    AppEnd pkg bugfix

--- MsSql/Packages/AppEnd/.sql
+++ MsSql/Packages/AppEnd/.sql
@@ -1,4 +1,4 @@
-EXEC Zz_DropTable 'AAA_Users_R_Roles';
+EXEC Zz_DropTable 'AAA_Users_Roles';
 EXEC Zz_DropTable 'AAA_Roles';
 EXEC Zz_DropTable 'AAA_Users';
 EXEC Zz_DropTable 'Common_ActivityLog';
@@ -8,5 +8,5 @@ EXEC Zzz_Deploy 'AppEnd/Common_ActivityLog.sql';
 EXEC Zzz_Deploy 'AppEnd/Common_BaseInfo.sql';
 EXEC Zzz_Deploy 'AppEnd/AAA_Roles.sql';
 EXEC Zzz_Deploy 'AppEnd/AAA_Users.sql';
-EXEC Zzz_Deploy 'AppEnd/AAA_Users_R_Roles.sql';
+EXEC Zzz_Deploy 'AppEnd/AAA_Users_Roles.sql';
 
