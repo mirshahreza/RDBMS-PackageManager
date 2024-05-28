09b5a334e899d3fd1b5ec4f6bac6847cac525a9c
Mohsen Mirshahreza <mirshahreza@outlook.com>
2024-05-28 15:18:18 +0330

    Bugfix

--- MsSql/Packages/AppEnd/AAA_Users_Roles.sql
+++ MsSql/Packages/AppEnd/AAA_Users_Roles.sql
@@ -3,13 +3,13 @@
 -- Create date: 2024-03-31
 -- Description:	Table to hold roles of users - AppEnd LowCode system
 -- =============================================
-CREATE TABLE [DBO].[AAA_Users_R_Roles](
+CREATE TABLE [DBO].[AAA_Users_Roles](
 	[Id] [int] IDENTITY(1000000,1) NOT NULL,
 	[CreatedBy] [int] NOT NULL,
 	[CreatedOn] [datetime] NOT NULL,
 	[UserId] [int] NOT NULL,
 	[RoleId] [int] NOT NULL,
- CONSTRAINT [PK_AppEnd_Users_R_Roles] PRIMARY KEY CLUSTERED 
+ CONSTRAINT [PK_AppEnd_Users_Roles] PRIMARY KEY CLUSTERED 
 (
 	[Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
